10c1b8de0aba9aa07c0c365f4e1792bb4a4f6e3b
brianokinyi <brianokinyi.bo@gmail.com>
2018-06-02 09:57:09 +0000

    Added getter methods

--- main.cpp
+++ main.cpp
@@ -6,12 +6,32 @@ using namespace std;
 class Employee
 {
     public:
+        string getFname() {
+            return fname;
+        }
+
+        string getLname() {
+            return lname;
+        }
+
+        string getGender() {
+            return gender;
+        }
+
+        string getAddress() {
+            return address;
+        }
+
+        string getJob_desc() {
+            return job_desc;
+        }
 
         
     private:
         string fname;
         string lname;
         string gender;
+        string address;
         string job_desc;
     protected:
 }
