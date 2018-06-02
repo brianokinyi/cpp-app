3a1a269cde1f4ae17fdbaf2be4aa7d1faea0d99f
brianokinyi <brianokinyi.bo@gmail.com>
2018-06-02 09:52:24 +0000

    Added a class

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 using namespace std;
 
+class Employee
+{
+    public:
+        
+    private:
+        string fname;
+        string lname;
+        string gender;
+        string job_desc;
+    protected:
+}
+
 int main() 
 {
-    cout << "Hello, World!";
     return 0;
 }
